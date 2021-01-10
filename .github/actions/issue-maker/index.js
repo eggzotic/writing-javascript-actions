d24455c0ca26e01d1f8e697c797446e5fa597ea2
Richard Shepherd <eggzotic@gmail.com>
2021-01-10 13:44:53 +0800

    corrected variable name bib -to- octokit

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -8,7 +8,7 @@ async function run() {
     const token = core.getInput("repo-token");
 
     //   const octokit = new github.GitHub(token);
-      const bib = new github.getOctokit(token);
+      const octokit = new github.getOctokit(token);
 
     const newIssue = await octokit.issues.create({
       repo: github.context.repo.repo,
